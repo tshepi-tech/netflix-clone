0795cc1c14a307e586847770155965b20503a069
tshepi-tech <tshepi.tech@gmail.com>
2022-05-15 00:42:30 +0200

    create movie form implemented

--- src/Admin/pages/MoviesAdmin.jsx
+++ src/Admin/pages/MoviesAdmin.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+//Project files
+import CreateTitle from "Admin/components/CreateTitle";
+import mediaData from "Admin/data/mediaData";
+import titleData from "Admin/data/titleData";
+import { useModal } from "state/ModalContext";
 
 export default function MoviesAdmin() {
-  return <div>MoviesAdmin</div>;
+  const { setModal } = useModal();
+  const createMovies = (
+    <CreateTitle titleData={titleData} mediaData={mediaData} />
+  );
+
+  return (
+    <div>
+      <button onClick={() => setModal(createMovies)}>Add Movie</button>
+    </div>
+  );
 }
