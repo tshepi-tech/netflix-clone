7924131da8590f2f97d279dc896172adecc0dfc5
tshepi-tech <tshepi.tech@gmail.com>
2022-05-14 20:47:35 +0200

    initial pages created

---
+++ src/Admin/pages/DocumentaryAdmin.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DocumentaryAdmin() {
+  return <div>DocumentaryAdmin</div>;
+}
---
+++ src/Admin/pages/SeasonsAdmin.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SeasonsAdmin() {
+  return <div>SeasonsAdmin</div>;
+}
