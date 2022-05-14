d3455c481704a37f8c72f6b4f508fb9961377d8c
tshepi-tech <tshepi.tech@gmail.com>
2022-05-15 00:51:49 +0200

    create series form done

---
+++ src/Admin/components/CreateSeries.jsx
@@ -0,0 +1,43 @@
+//NPM packages
+import { useState } from "react";
+
+//Project files
+import InputTitle from "./InputTitle";
+import InputMedia from "./InputMedia";
+import { useModal } from "state/ModalContext";
+
+export default function CreateSeries({ seriesData, mediaData }) {
+  //Global state
+  const { setModal } = useModal();
+  // Local state
+  const [form, setForm] = useState({});
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    resetForm();
+  }
+
+  function resetForm() {
+    setModal(null);
+  }
+
+  // Components
+  const InputFields = seriesData.map((item) => (
+    <InputTitle key={item.key} setup={item} state={[form, setForm]} />
+  ));
+  const InputImages = mediaData.map((item) => (
+    <InputMedia key={item.key} setup={item} state={[form, setForm]} />
+  ));
+
+  return (
+    <form className="form" onSubmit={onSubmit}>
+      <h2>Create Title</h2>
+      {InputFields}
+      {InputImages}
+      <button className="button primary">Submit</button>
+      <button className="button secondary" onClick={resetForm}>
+        Cancel
+      </button>
+    </form>
+  );
+}
--- src/Admin/pages/SeriesAdmin.jsx
+++ src/Admin/pages/SeriesAdmin.jsx
@@ -1,5 +1,18 @@
-import React from "react";
+//Project files
+import CreateSeries from "Admin/components/CreateSeries";
+import mediaData from "Admin/data/mediaData";
+import seriesData from "Admin/data/seriesData";
+import { useModal } from "state/ModalContext";
 
-export default function SeriesAdmin() {
-  return <div>SeriesAdmin</div>;
+export default function DocumentaryAdmin() {
+  const { setModal } = useModal();
+  const createDocumentary = (
+    <CreateSeries seriesData={seriesData} mediaData={mediaData} />
+  );
+
+  return (
+    <div>
+      <button onClick={() => setModal(createDocumentary)}>Add Series</button>
+    </div>
+  );
 }
