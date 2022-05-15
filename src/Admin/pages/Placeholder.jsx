4a586620abfa713da2ef72426dcd8b3969edefde
tshepi-tech <tshepi.tech@gmail.com>
2022-05-15 13:56:17 +0200

    page for placeholder when there is no item

--- src/Admin/pages/Placeholder.jsx
+++ src/Admin/pages/Placeholder.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+// Project files
+import emptyTV from "assets/images/emptyTV.png";
 
 export default function Placeholder() {
-  return <div>Placeholder</div>;
+  return (
+    <article>
+      <img src={emptyTV} alt="A TV with wave lines on screen" />
+      <p>There is no content yet. Please add</p>
+    </article>
+  );
 }
