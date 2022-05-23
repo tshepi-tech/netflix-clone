8f14347b04053fdfbc453f0dc878daab0b122cb5
tshepi-tech <tshepi.tech@gmail.com>
2022-05-23 18:13:18 +0200

    season toggle works. shows episodes

--- src/components/app/SelectField.jsx
+++ src/components/app/SelectField.jsx
@@ -1,31 +1,31 @@
 //NPM package
 import { useEffect, useState } from "react";
+
 //Project files
 import EpisodesItem from "components/users/EpisodesItem";
 import { readCollection } from "scripts/firestore";
 export default function SelectField({ title }) {
   const [episodes, setEpisodes] = useState([]);
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState(1);
 
   //Properties
   function onSelect(event) {
     setValue(event.target.value);
   }
-  const seasonNo = value;
-
-  const episodePath = `netflix/series/content/${title.id}/content/${seasonNo}/content`;
+  const episodePath = `netflix/series/content/${title.id}/content/${value}/content`;
+  console.log(episodePath);
   useEffect(() => {
     async function loadData(episodePath) {
       const data = await readCollection(episodePath);
       setEpisodes(data);
     }
-
     loadData(episodePath);
-  }, []);
+  }, [value]);
 
   const EpisodeList = episodes.map((episode) => (
     <EpisodesItem key={episode.id} episode={episode} />
   ));
+
   return (
     <div>
       <label for="seasons">Seasons</label>
@@ -37,7 +37,9 @@ export default function SelectField({ title }) {
         <option value="5">Season 5</option>
       </select>
       Season: {value}
-      {EpisodeList}
+      <br></br>
+      {episodes.length === 0 && "More episodes coming soon"}
+      {episodes.length > 0 && EpisodeList}
     </div>
   );
 }
