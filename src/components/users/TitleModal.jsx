e51de5018ca3a9e86d722de374ea5b2b054d9e47
tshepi-tech <tshepi.tech@gmail.com>
2022-05-29 12:07:22 +0200

    Title modal styled

--- src/components/users/TitleModal.jsx
+++ src/components/users/TitleModal.jsx
@@ -1,6 +1,8 @@
 //Project Files
 import { useModal } from "state/ModalContext";
 import VideoPlay from "./VideoPlay";
+import foxcatcher from "assets/images/foxcatcher.jpg";
+import straightOC from "assets/images/straightOC.jpg";
 
 export default function TitleModal({ title }) {
   const {
@@ -30,22 +32,35 @@ export default function TitleModal({ title }) {
       >
         Play
       </button>
-      <div className="container1">
-        <span>{year}</span>
-        <span>{age}</span>
-        <span>{duration}</span>
-      </div>
+      <div className="title__info">
+        <section className="description__all">
+          <div className="container1">
+            <span className="year">{year}</span>
+            <span className="age">{age}</span>
+            <span className="duration">{duration}</span>
+          </div>
+          <div className="container2">
+            <p className="description">{text}</p>
+          </div>
+        </section>
 
-      {/* <h1>{name}</h1> */}
-      <section className="description__all">
-        <div className="container2">
-          <p className="description">{text}</p>
-        </div>
         <div className="container3">
-          <p>Cast: {cast}</p>
-          <p>Genre: {genre}</p>
-          <p>This show is :{theme}</p>
+          <span>
+            <p className="detail">Cast:</p> <p className="detail2">{cast}</p>
+          </span>
+          <span className="genre">
+            <p className="detail">Genre:</p> <p className="detail2"> {genre}</p>
+          </span>
+          <span className="theme">
+            <p className="detail">This show is: </p>
+            <p className="detail2">{theme}</p>
+          </span>
         </div>
+      </div>
+      <h2 className="more">More Like This</h2>
+      <section className="extra__images">
+        <img src={foxcatcher} alt="foxcatcher thumbnail" />
+        <img src={straightOC} alt="straight outta compton tumbnail" />
       </section>
     </section>
   );
